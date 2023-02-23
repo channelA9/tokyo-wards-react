7e244543e206e36617e89dd99e4bbe3ba909e8cb
channel9a <shaun.colegado@gmail.com>
2023-02-22 23:11:28 -0800

    Reformatted with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Board from "./components/board";
 
 function App() {
   return (
-    <div className="App font-sans min-w-fit p-4 text-white">
-      <h1 className=" text-4xl font-black m-8">The Special Wards of Tokyo (東京)</h1>
-      <h2　className=' text-xl'>🌆 All 23 Special Wards 🏙️</h2>
+    <div className="App min-w-fit p-4 font-sans text-white">
+      <h1 className=" m-8 text-4xl font-black">
+        The Special Wards of Tokyo (東京)
+      </h1>
+      <h2 className=" text-xl">🌆 All 23 Special Wards 🏙️</h2>
 
-      <div className='flex text-center place-items-center divide-x-2 p-8 my-2 '>
-        <h2 className='text-lg font-bold w-1/2'></h2>
-        <h2 className='text-sm w-1/2' ></h2>
+      <div className="my-2 flex place-items-center divide-x-2 p-8 text-center ">
+        <h2 className="w-1/2 text-lg font-bold"></h2>
+        <h2 className="w-1/2 text-sm"></h2>
       </div>
 
       <Board />
